7e7b57bf8816e9fa58f362046ae90231d404a3f9
JauniusPinelis <JauniusPinelis@gmail.com>
2016-12-18 17:12:25 +0200

    Started coding Edit Project functionality

--- ProgressTracker/Controllers/ProjectTimeController.cs
+++ ProgressTracker/Controllers/ProjectTimeController.cs
@@ -24,6 +24,27 @@ namespace ProgressTracker.Controllers
             var project = new ProjectModel();
             return View(project);
         }
+        
+        //public ActionResult Edit(long id)
+        //{
+        //    var project = context.Projects.Single(x => x.Id == id);
+        //    if (project == null)
+        //        return HttpNotFound();
+        //    return View(project);
+        //}
+        
+        public ActionResult Edit(long id, ProjectModel project)
+        {
+            var _project = context.Projects.Single(x => x.Id == id);
+
+            _project.Name = project.Name;
+            _project.Description = project.Description;
+            _project.TimeSpentInMinutes = project.TimeSpentInMinutes;
+
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
 
         public ActionResult Submit(ProjectModel project)
         {
