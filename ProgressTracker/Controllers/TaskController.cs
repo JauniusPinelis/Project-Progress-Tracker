50e346f6c1204cef19bd43cf181b843b0f298b37
JauniusPinelis <JauniusPinelis@gmail.com>
2016-12-19 16:42:38 +0200

    Started adding Task Controller

---
+++ ProgressTracker/Controllers/TaskController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProgressTracker.Context;
+using ProgressTracker.Models;
+
+namespace ProgressTracker.Controllers
+{
+    public class TaskController : Controller
+    {
+        private ProjectDbContext db = new ProjectDbContext();
+
+        // GET: Task
+        public ActionResult Index()
+        {
+            return View(db.TaskModels.ToList());
+        }
+
+        // GET: Task/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskModel taskModel = db.TaskModels.Find(id);
+            if (taskModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskModel);
+        }
+
+        // GET: Task/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Task/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,MinutesSpent")] TaskModel taskModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TaskModels.Add(taskModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(taskModel);
+        }
+
+        // GET: Task/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskModel taskModel = db.TaskModels.Find(id);
+            if (taskModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskModel);
+        }
+
+        // POST: Task/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,MinutesSpent")] TaskModel taskModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taskModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taskModel);
+        }
+
+        // GET: Task/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaskModel taskModel = db.TaskModels.Find(id);
+            if (taskModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taskModel);
+        }
+
+        // POST: Task/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            TaskModel taskModel = db.TaskModels.Find(id);
+            db.TaskModels.Remove(taskModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProgressTracker/Models/TaskModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProgressTracker.Models
+{
+    [Table("task")]
+    public class TaskModel
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long MinutesSpent { get; set; }
+    }
+}
